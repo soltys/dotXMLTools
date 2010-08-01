757f30501d0f0383888d31e16576a133a54b77c2
soltys <soltys0@gmail.com>
2010-08-01 19:32:08 +0200

    init commit

---
+++ XML2List/PathCounter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XML2List
+{
+    public class PathCounter : Dictionary<string, int>
+    {
+    }
+}
