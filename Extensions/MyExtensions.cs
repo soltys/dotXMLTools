2f57b77050ce6de950d068f75ca147d522841332
Paweł Sołtysiak <soltys@uznam.org>
2010-08-02 08:45:32 +0200

    Extensions

---
+++ Extensions/MyExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MyExtensions
+{
+    public static class MyExtensions
+    {
+        public static string DeleteLastCharacter(this string str)
+        {
+            return str.Substring(0, str.Length - 1);
+        }
+    }
+}
