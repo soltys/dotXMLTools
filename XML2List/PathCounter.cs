a4f4e01d92ccaa048486dafed4e7ccbf5a40bfe4
Paweł Sołtysiak <soltys@uznam.org>
2010-08-02 08:46:11 +0200

    pathcounter

--- XML2List/PathCounter.cs
+++ XML2List/PathCounter.cs
@@ -7,5 +7,20 @@ namespace XML2List
 {
     public class PathCounter : Dictionary<string, int>
     {
+        public void Add(string stringToAdd)
+        {
+            if (ContainsKey(stringToAdd))
+            {
+                base[stringToAdd]++;
+            }
+            else
+            {
+                Add(stringToAdd, 1);
+            }
+        }
+
+        
+
+        
     }
 }
