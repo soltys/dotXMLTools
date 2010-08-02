3b88f158516ae1fc44a6adda2856db1ef7be18e2
Paweł Sołtysiak <soltys@uznam.org>
2010-08-02 08:46:44 +0200

    testing program

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -2,13 +2,21 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Xml.Linq;
+using XML2List;
 namespace TestingConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
+            XElement root = XElement.Load(@"c:\pawel\xmlText\ulice.xml");
+            PathCollector pc = new PathCollector(root);
+
+            foreach (var i in pc.PathCounter)
+            {
+                Console.WriteLine(i.Key + "  =  " + i.Value);
+            }
         }
     }
 }
