ea4af12e027487c90bb22f63d0efacf412c5fafd
Paweł Sołtysiak <soltys@uznam.org>
2010-08-03 10:45:54 +0200

    classes for selecting items from file

---
+++ XML2List/AttributeSelector.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XML2List
+{
+    class AttributeSelector : IListSelect
+    {
+        public IEnumerable<XElement> SelectItems(IEnumerable<IEnumerable<XElement>> listofXElements)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
