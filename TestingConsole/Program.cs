ef2ae5c8a30af938d4ba3338cd73f20efa26ecba
Paweł Sołtysiak <soltys@uznam.org>
2010-08-03 21:08:57 +0200

    testing program

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -17,7 +17,27 @@ namespace TestingConsole
             {
                 Console.WriteLine(s.Key + " " + s.Value);
             }
-            
+            var l = new  List<IListSelect>();
+            l.Add(new ElementsSelector("teryt"));
+            l.Add(new ElementsSelector("catalog"));
+            l.Add(new ElementsSelector("row"));
+            l.Add(new ElementsSelector("col"));
+            l.Add(new AttributeValueSelector("name","NAZWA_1"));
+
+            IEnumerable<XElement> a = root.Elements();
+
+            for (int index = 1; index < l.Count; index++)
+            {
+                var elementsSelector = l[index];
+                Console.WriteLine("using a " + elementsSelector.Name);
+                a = elementsSelector.SelectItems(a);
+            }
+
+
+            foreach (var xElement in a)
+            {
+                Console.WriteLine(xElement.Value);
+            }
         }
     }
 }
