1c737811cdeb4c38cd56496d93e45c4c993bfa24
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 22:57:21 +0200

    new stub for parsepath

--- XML2List/PathParser.cs
+++ XML2List/PathParser.cs
@@ -2,16 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using MyExtensions;
+using XML2List.Interface;
 namespace XML2List
 {
     class PathParser
     {
-        public IEnumerable<IListSelect> ParsePath(string pathToParse)
+        public CommandLists ParsePath(string pathToParse)
+        {
+            string[] pathSplitElements = pathToParse.Split('/');
+            CommandLists commands = new CommandLists();
+
+            foreach(string pathPart in pathSplitElements.Where( x=> x.IsNotEmptyOrNull()))
+            {
+                IElementGroupSelect groupCommand = getGroupCommand(pathPart);
+                IItemSelect itemCommand = getItemCommand(pathPart);
+
+                commands.GroupSelectCommands.Add(groupCommand);
+                commands.ItemSelectCommands.Add(itemCommand);
+            }
+
+            return commands;
+        }
+
+        private IItemSelect getItemCommand(string pathPart)
         {
             return null;
         }
 
-
+        private IElementGroupSelect getGroupCommand(string pathPart)
+        {
+            
+            return null;
+        }
     }
 }
