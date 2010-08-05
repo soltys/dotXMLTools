5572d3993dc148f3880ad6914c3ab2d3a1fe9f73
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 22:29:36 +0200

    Test project rename

---
+++ DotXMLToolsTest/ExtensionsTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MyExtensions;
+
+namespace ExtensionsTest
+{
+    [TestClass]
+    public class ExtensionsTest
+    {
+        [TestMethod]
+        public void DeleteLastCharacterTest()
+        {
+            string input = "abc";
+            string expected = "ab";
+            string output;
+            output = input.DeleteLastCharacter();
+            Assert.AreEqual(output, expected);
+        }
+    }
+}
