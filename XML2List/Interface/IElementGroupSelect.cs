d2ca70d75e382a699ecdbca23c4945704a10a421
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 22:51:59 +0200

    namespace changed

--- XML2List/Interface/IElementGroupSelect.cs
+++ XML2List/Interface/IElementGroupSelect.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
-namespace XML2List
+namespace XML2List.Interface
 {
     public interface IElementGroupSelect
     {
