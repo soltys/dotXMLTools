dbd70e4c9d674e2ed653927d493f7ad3c20d2671
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 22:05:39 +0200

    new interface

---
+++ XML2List/Interface/IItemSelect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+
+namespace XML2List.Interface
+{
+    interface IItemSelect
+    {
+        bool SelectItem(XElement item);
+    }
+}
