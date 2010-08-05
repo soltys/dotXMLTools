df10f585dc26cae8ea05139a3d7648e78a622fae
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 22:43:58 +0200

    Added class CommandLists for combine two kinds of transformation on XML tree

---
+++ XML2List/CommandLists.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using XML2List.Interface;
+namespace XML2List
+{
+    class CommandLists
+    {
+        public List<IElementGroupSelect> GroupSelectCommands { get; set; }
+
+        public List<IItemSelect> ItemSelectCommands { get; set; }
+    }
+}
