97df2097e53c94508fec828840152e73633ffdca
Paweł Sołtysiak <soltys@uznam.org>
2010-08-05 20:51:37 +0200

    new stub file

---
+++ XML2List/PathParser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XML2List
+{
+    class PathParser
+    {
+        public IEnumerable<IListSelect> ParsePath(string pathToParse)
+        {
+            return null;
+        }
+
+
+    }
+}
