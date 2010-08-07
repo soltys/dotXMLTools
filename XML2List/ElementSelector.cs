e5458679795e6840e276b4df9d677972bc1b5598
Paweł Sołtysiak <soltys@uznam.org>
2010-08-07 23:16:18 +0200

    changed elementSelector to new interface

--- XML2List/ElementSelector.cs
+++ XML2List/ElementSelector.cs
@@ -4,26 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using XML2List.Interface;
-
+using MyExtensions;
 namespace XML2List
 {
     public class ElementSelector:IItemSelect
     {
-        private string _elementName;
+        private string _elementPath;
 
-        public ElementSelector(string elementName)
+        public ElementSelector(string elementPath)
         {
-            this._elementName = elementName;
+            this._elementPath = elementPath;
         }
 
         public XElement SelectItem(XElement item)
         {
-            return null;
+            //TODO change this in something more pretty
+            string [] elementsPaths = _elementPath.Split('/').Where(x => x.IsNotEmptyOrNull()).ToArray();
+            IEnumerable<XElement> collection = item.Elements(elementsPaths[0]);
+            
+            
+            for (int index = 1; index < elementsPaths.Length; index++)
+            {
+                collection = collection.Elements(elementsPaths[index]);
+            }
+            
+            return collection.First();
         }
 
         public string Value
         {
-            get { return _elementName; }
+            get { return _elementPath; }
         }
     }
 }
