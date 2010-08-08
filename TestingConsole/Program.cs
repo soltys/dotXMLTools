db6e2e11aa6028f74ab0e775c5d93a96dc4c3304
Paweł Sołtysiak <soltys@uznam.org>
2010-08-08 11:46:16 +0200

    First Console Program

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -1,43 +1,141 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using XML2List;
+using XML2List.Interface;
+
 namespace TestingConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            XDocument root = XDocument.Load(@"c:\pawel\xmlText\ulice.xml");
+#if true
+            XDocument root = XDocument.Load(@"d:\pawel\xmlText\baza.xml");
             PathFinder pc = new PathFinder(root.Root);
+            Dictionary<string,int> selection = new Dictionary<string, int>();
 
             foreach (var s in pc.PathCounter)
             {
-                Console.WriteLine(s.Key + " " + s.Value);
+                selection.Add(s.Key,0);
             }
-            var l = new  List<IListSelect>();
-            l.Add(new ElementsSelector("teryt"));
-            l.Add(new ElementsSelector("catalog"));
-            l.Add(new ElementsSelector("row"));
-            l.Add(new ElementsSelector("col"));
-            l.Add(new AttributeValueSelector("name","NAZWA_1"));
+            ConsoleKeyInfo cki = new ConsoleKeyInfo();
+            int position = 0;
+            
+            do
+            {
+                
+                Console.Clear();
+                PrintOutView(pc, selection);
+                Console.CursorLeft = 1;
+                Console.CursorTop = position;
+                cki = Console.ReadKey(true);
+                switch (cki.Key)
+                {
+                    case ConsoleKey.Enter:
+                    case ConsoleKey.Spacebar:
+                        selection[pc.PathCounter.Take(position + 1).Last().Key] = 1;
+                        break;
+                    case ConsoleKey.UpArrow:
+                        if (position > 0)
+                        {
+                            position--;
+                        }
+                        break;
+                        
+                    case ConsoleKey.DownArrow:
+                        position++;
+                        break;
+                        
 
+                }
+            } while (cki.Key != ConsoleKey.S);
+
+#endif
+            PathParser pp = new PathParser();
+
+            string[] aa = getSelectedLines(selection);
+            CommandLists cl = pp.ParsePaths(aa);
+            
+            Console.Clear();
+            
+            foreach (var command in cl.GroupSelectCommands)
+            {
+                Console.WriteLine(command.Value);
+                Console.WriteLine(command.ToString());
+                Console.WriteLine("----");
+            }
+            
+
+
+            foreach (var command in cl.ItemSelectCommands)
+            {
+                Console.WriteLine(command.Value);
+                Console.WriteLine(command.ToString());
+                Console.WriteLine("----");
+            }
+
+#if true       
             IEnumerable<XElement> a = root.Elements();
 
-            for (int index = 1; index < l.Count; index++)
+            for (int index = 1; index < cl.GroupSelectCommands.Count; index++)
             {
-                var elementsSelector = l[index];
-                Console.WriteLine("using a " + elementsSelector.Name);
+                var elementsSelector = cl.GroupSelectCommands[index];
+                Console.WriteLine("using a " + elementsSelector.Value);
                 a = elementsSelector.SelectItems(a);
             }
 
 
-            foreach (var xElement in a)
+            foreach (var xElement in a.Take(10))
             {
-                Console.WriteLine(xElement.Value);
+                foreach (var itemCommand in cl.ItemSelectCommands)
+                {
+                    XElement xe = itemCommand.SelectItem(xElement);
+                    if(xe != null)
+                    {
+                        Console.WriteLine(xe.Name + " -> " + xe.Value);
+                    }
+                   
+                }
+            //    if(aa.Length == 1)
+            //    Console.WriteLine(xElement.Name + " -> " + xElement.Value);
+                Console.WriteLine("---");
+            }
+#endif
+
+        }
+
+        private static string[] getSelectedLines(Dictionary<string, int> selection)
+        {
+            List<string > listToReturn = new List<string>();
+            foreach (var i in selection.Where(x=>x.Value == 1))
+            {
+                listToReturn.Add(i.Key);
+            }
+            return listToReturn.ToArray();
+        }
+
+        private static void PrintOutView(PathFinder pc, Dictionary<string, int> selection)
+        {
+            foreach (var car in pc.PathCounter)
+            {
+                Console.Write("[");
+                switch (selection[car.Key])
+                {
+                    case 0:
+                        Console.Write(" ");
+                        break;
+                    case 1:
+                        Console.Write("&");
+                        break;
+                }
+                Console.Write("] " + car.Key + " (" + car.Value + ")" + Environment.NewLine);
             }
         }
+
+        
     }
 }
