c4dc46a6d4241037ea7dc9c5e5f70d8a4d9fdc04
Paweł Sołtysiak <soltys@uznam.org>
2010-08-12 14:43:13 +0200

    new class stub

---
+++ XML2List/PathElementsTools.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace XML2List
+{
+    /// <summary>
+    /// Collections of static methods which combines Path and Elements in doument tree
+    /// </summary>
+    static class  PathElementsTools
+    {
+
+    }
+}
