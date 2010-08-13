08bad98effc90027d8ac46e09c5a7d586ebb15cb
Paweł Sołtysiak <soltys@uznam.org>
2010-08-13 09:58:57 +0200

    Program for testing purposes

--- TestingConsole/Program.cs
+++ TestingConsole/Program.cs
@@ -4,40 +4,67 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using XML2List;
+using XML2List.Interface;
+
 namespace TestingConsole
 {
     class Program
     {
         static void Main(string[] args)
         {
-            XDocument root = XDocument.Load(@"c:\pawel\xmlText\ulice.xml");
+            XDocument root = XDocument.Load(@"d:\pawel\xmlText\ulice.xml");
             PathFinder pc = new PathFinder(root.Root);
 
             foreach (var s in pc.PathCounter)
             {
                 Console.WriteLine(s.Key + " " + s.Value);
             }
-            var l = new  List<IListSelect>();
-            l.Add(new ElementsSelector("teryt"));
-            l.Add(new ElementsSelector("catalog"));
-            l.Add(new ElementsSelector("row"));
-            l.Add(new ElementsSelector("col"));
-            l.Add(new AttributeValueSelector("name","NAZWA_1"));
+
+            string lineToParse = "/teryt/catalog/row/col[name=WOZ]";
+            PathParser pp = new PathParser();
+            CommandLists cl = pp.ParsePath(lineToParse);
+
+
+            foreach (var command in cl.GroupSelectCommands)
+            {
+                Console.WriteLine(command.Value);
+                Console.WriteLine(command.ToString());
+                Console.WriteLine("----");
+            }
+
+            foreach (var command in cl.ItemSelectCommands)
+            {
+                Console.WriteLine(command.Value);
+                Console.WriteLine(command.ToString());
+                Console.WriteLine("----");
+            }
+
+            Console.WriteLine("Hurray!");
+            Console.ReadKey();
+
+            /*
 
             IEnumerable<XElement> a = root.Elements();
 
             for (int index = 1; index < l.Count; index++)
             {
                 var elementsSelector = l[index];
-                Console.WriteLine("using a " + elementsSelector.Name);
+                Console.WriteLine("using a " + elementsSelector.Value);
                 a = elementsSelector.SelectItems(a);
             }
 
 
             foreach (var xElement in a)
             {
-                Console.WriteLine(xElement.Value);
+                foreach (var element in xElement.Elements("col")) 
+                    //spianie w jedno naprzykład w postać XML'A
+                {
+                    Console.WriteLine(element.Name + " " + element.Value);    
+                }
+                
+                Console.WriteLine("---");
             }
+            */
         }
     }
 }
